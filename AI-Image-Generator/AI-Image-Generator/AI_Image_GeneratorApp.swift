57dca712f44d5ff74cca2f86f8e17c01e88377b8
Osman Canboğa <osmancanboga@hotmail.com>
2023-01-07 23:04:18 +0300

    AI Image Generator with Openai + DALL E

---
+++ AI-Image-Generator/AI-Image-Generator/AI_Image_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Image_GeneratorApp.swift
+//  AI-Image-Generator
+//
+//  Created by osman canboga on 7.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AI_Image_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-Image-Generator/AI-Image-Generator/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  AI-Image-Generator
+//
+//  Created by osman canboga on 7.01.2023.
+//
+import OpenAIKit
+import SwiftUI
+
+final class ViewModel: ObservableObject{
+    private var openai: OpenAI?
+    func setup(){
+        openai = OpenAI(Configuration(organization: "Personal", apiKey: "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"))
+    }
+    
+    func generateImage(prompt: String) async -> UIImage?{
+        guard let openai = openai else{
+            return nil
+        }
+        
+        do{
+           let params = ImageParameters(
+            prompt: prompt,
+            resolution: .medium,
+            responseFormat: .base64Json
+           )
+            let result = try await openai.createImage(parameters: params)
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            return image
+        }
+        catch{
+            print(String(describing: error))
+            return nil
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var image : UIImage?
+    
+    var body: some View {
+        NavigationView {
+            VStack{
+                if let image = image{
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        .frame(width: 500,height: 500)
+                }
+                else
+                {
+                    Text("Type prompt to generate image!")
+                }
+                Spacer()
+                
+                TextField("Type prompt here..", text:$text)
+                    .padding()
+                Button("Generate!"){
+                    Task{
+                        if !text.trimmingCharacters(in: .whitespaces).isEmpty{
+                            let result = await viewModel.generateImage(prompt: text)
+                            if result == nil{
+                                print("Failed to get image")
+                            }
+                            self.image = result
+                        }
+                        
+                    }
+                }
+                
+            }
+            .navigationTitle("DALL-E Image Generator")
+            .onAppear{
+                viewModel.setup()
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
